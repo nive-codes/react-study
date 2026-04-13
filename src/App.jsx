9b149973e557f2e7efc8f91c6d056495e09136ea
nive.codes <nive.codes@gmail.com>
2026-04-13 14:13:02 +0900

    Excample 별도 컴포넌트 생성 및 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,8 @@
-import {useState, Fragment} from 'react'
-
 import Header from './components/Header/Header.jsx';
 import CoreConcepts from './components/CoreConcepts.jsx';
-import TabButton from './components/TabButton.jsx';
-import {EXAMPLES} from './data.js';
-
-
-
+import Examples from './components/Excamples.jsx';
 
 function App() {
-
-  //component 함수에서 최상위에서 사용(if, for로 감싸지 말 것.), handleSelected 같은 내부 함수에서 호출하면 x.컴포넌트 함수에서 즉시 사용해야함.
-  //첫번쨰 지정하는 것은 현재 상태 값.
-  const [selectedTopic, setSelectedTopic] = useState(); 
-  
-
-  // TabButton 컴포넌트로 전달할 function(TabButton 컴포넌트에서 onClick 이벤트핸들러 통해 실행 된다.)
-  // onSelect라는 props로 전달
-  function handleSelect(selectedButton) {
-    
-    setSelectedTopic(selectedButton); // 컴포넌트 함수에 있는 useSate의 두번쨰 배열 -> 변경값 적용
-
-  }
-
-  console.log('APP COMPONENT EXECUTING');
-
-  //변수를 활용한 state 선택 없을때 처리(동적)
-  let tabContent = <p>Please select a topic</p>;
-
-  if(selectedTopic) {
-    tabContent = (
-       <div id="tab-content">
-                <h3>{EXAMPLES[selectedTopic].title}</h3>
-                <p>{EXAMPLES[selectedTopic].description}</p>
-                <pre>
-                  <code>
-                    {EXAMPLES[selectedTopic].code}
-                  </code>
-                </pre>
-              </div>
-    )
-  }
 
   return (
     // div 하나를 반환해야함.(요소 하나만을 반환해야됨. javascript에서도 return 2개 할 수 없으니까.)
@@ -52,24 +14,7 @@ function App() {
       <Header />
       <main>
         <CoreConcepts/>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-           
-             {/* 식별자를 통해 data.js의 EXAMPLES의 객체와 일치 */}
-            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton> 
-            <TabButton isSelected={selectedTopic === 'jsx'} onSelect={function() {handleSelect('jsx')}}>JSX</TabButton> 
-            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Porps</TabButton> 
-            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>State</TabButton> 
-            {/* CoreConcept와 같은 방식 */}
-            {/* <TabButton label="Components" /> */}
-            
-          </menu>
-          {/* 변수에 담아서 쓰는 방법 */}
-          {tabContent}
-
-          
-        </section>
+        <Examples/>
       </main>
     </>
     // </Fragment>
---
+++ src/components/Excamples.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { EXAMPLES } from "../data.js";
+import TabButton from "./TabButton.jsx";
+
+export default function Examples(){
+
+  //component 함수에서 최상위에서 사용(if, for로 감싸지 말 것.), handleSelected 같은 내부 함수에서 호출하면 x.컴포넌트 함수에서 즉시 사용해야함.
+  //첫번쨰 지정하는 것은 현재 상태 값.
+  const [selectedTopic, setSelectedTopic] = useState(); 
+
+  // TabButton 컴포넌트로 전달할 function(TabButton 컴포넌트에서 onClick 이벤트핸들러 통해 실행 된다.)
+  // onSelect라는 props로 전달
+  function handleSelect(selectedButton) {
+    
+    setSelectedTopic(selectedButton); // 컴포넌트 함수에 있는 useSate의 두번쨰 배열 -> 변경값 적용
+
+  }
+
+  //변수를 활용한 state 선택 없을때 처리(동적)
+  let tabContent = <p>Please select a topic</p>;
+
+  if(selectedTopic) {
+    tabContent = (
+       <div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                  <code>
+                    {EXAMPLES[selectedTopic].code}
+                  </code>
+                </pre>
+              </div>
+    )
+  }
+
+    return (
+        <section id="examples">
+            <h2>Examples</h2>
+            <menu>
+            
+                {/* 식별자를 통해 data.js의 EXAMPLES의 객체와 일치 */}
+            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton> 
+            <TabButton isSelected={selectedTopic === 'jsx'} onSelect={function() {handleSelect('jsx')}}>JSX</TabButton> 
+            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Porps</TabButton> 
+            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>State</TabButton> 
+            {/* CoreConcept와 같은 방식 */}
+            {/* <TabButton label="Components" /> */}
+            
+            </menu>
+            {/* 변수에 담아서 쓰는 방법 */}
+            {tabContent}
+        </section>
+    )
+}
