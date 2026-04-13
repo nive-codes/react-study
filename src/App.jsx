cc39c45c753a52032eb6467c40ee83f629bb7da9
nive.codes <nive.codes@gmail.com>
2026-04-13 14:02:29 +0900

    Core Concepts 별도컴포넌트로 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import {useState, Fragment} from 'react'
 
-import {CORE_CONCEPTS} from './data.js';
 import Header from './components/Header/Header.jsx';
-import CoreConcept from './components/CoreConcept.jsx';
+import CoreConcepts from './components/CoreConcepts.jsx';
 import TabButton from './components/TabButton.jsx';
 import {EXAMPLES} from './data.js';
 
@@ -52,20 +51,7 @@ function App() {
     <>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {/* 동적으로 있는 배열 만큼 처리 */}
-            {/* map 메서드를 활용하여 jsx로 데이터를 변환해서 표시 */}
-            {/* jsx라는게 script파일 내 html이 포함된 소스 */}
-            {/* 고유한 키를 가지고 있는게 좋음. => item들의 구별을 위해 */}
-            {CORE_CONCEPTS.map((conceptItem) => (
-              <CoreConcept key={conceptItem.title} {...conceptItem}/>
-            ))}
-           
-          </ul>
-
-        </section>
+        <CoreConcepts/>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
